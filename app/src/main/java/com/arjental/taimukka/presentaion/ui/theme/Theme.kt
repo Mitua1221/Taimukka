8bff4e174d1583ccf8d830729f27a64c30229cb9
dkobyakov <kobyakovdi@taxcom.ru>
2023-01-28 23:32:14 +0300

    added logo

--- app/src/main/java/com/arjental/taimukka/presentaion/ui/theme/Theme.kt
+++ app/src/main/java/com/arjental/taimukka/presentaion/ui/theme/Theme.kt
@@ -1,41 +1,49 @@
 package com.arjental.taimukka.presentaion.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.colorResource
 import androidx.core.view.ViewCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+@Composable
+private fun produceColorScheme(): ColorScheme {
+    return ColorScheme(
+        primary = colorResource(id = com.arjental.taimukka.R.color.primary),
+        onPrimary = colorResource(id = com.arjental.taimukka.R.color.onPrimary),
+        primaryContainer = colorResource(id = com.arjental.taimukka.R.color.primaryContainer),
+        onPrimaryContainer = colorResource(id = com.arjental.taimukka.R.color.onPrimaryContainer),
+        inversePrimary = colorResource(id = com.arjental.taimukka.R.color.inversePrimary),
+        secondary = colorResource(id = com.arjental.taimukka.R.color.secondary),
+        onSecondary = colorResource(id = com.arjental.taimukka.R.color.onSecondary),
+        secondaryContainer = colorResource(id = com.arjental.taimukka.R.color.secondaryContainer),
+        onSecondaryContainer = colorResource(id = com.arjental.taimukka.R.color.onSecondaryContainer),
+        tertiary = colorResource(id = com.arjental.taimukka.R.color.tertiary),
+        onTertiary = colorResource(id = com.arjental.taimukka.R.color.onTertiary),
+        tertiaryContainer = colorResource(id = com.arjental.taimukka.R.color.tertiaryContainer),
+        onTertiaryContainer = colorResource(id = com.arjental.taimukka.R.color.onTertiaryContainer),
+        background = colorResource(id = com.arjental.taimukka.R.color.background),
+        onBackground = colorResource(id = com.arjental.taimukka.R.color.onBackground),
+        surface = colorResource(id = com.arjental.taimukka.R.color.surface),
+        onSurface = colorResource(id = com.arjental.taimukka.R.color.onSurface),
+        surfaceVariant = colorResource(id = com.arjental.taimukka.R.color.surfaceVariant),
+        onSurfaceVariant = colorResource(id = com.arjental.taimukka.R.color.onSurfaceVariant),
+        surfaceTint = colorResource(id = com.arjental.taimukka.R.color.surfaceTint),
+        inverseSurface = colorResource(id = com.arjental.taimukka.R.color.inverseSurface),
+        inverseOnSurface = colorResource(id = com.arjental.taimukka.R.color.inverseOnSurface),
+        error = colorResource(id = com.arjental.taimukka.R.color.error),
+        onError = colorResource(id = com.arjental.taimukka.R.color.onError),
+        errorContainer = colorResource(id = com.arjental.taimukka.R.color.errorContainer),
+        onErrorContainer = colorResource(id = com.arjental.taimukka.R.color.onErrorContainer),
+        outline = colorResource(id = com.arjental.taimukka.R.color.outline),
+        outlineVariant = colorResource(id = com.arjental.taimukka.R.color.outlineVariant),
+        scrim = colorResource(id = com.arjental.taimukka.R.color.scrim),
+    )
+}
 
 @Composable
 fun TaimukkaTheme(
@@ -48,13 +56,12 @@ fun TaimukkaTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> produceColorScheme()
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-           // (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            // (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
         }
     }
